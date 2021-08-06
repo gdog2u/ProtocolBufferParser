9a62164e27eeef247aad3448cadd0f1e1720de1a
Geoffrey Hudson <ghudson@dorothylane.com>
2021-08-06 14:50:09 -0400

    Add varint parsing and foundation for main parser
    I got the general flow for the main parsing function down. Shift bytes
    off the main byte array until it is empty, while also passing the byte
    array to the various datatype parsers to be shifted further.
    Added the varint parser.

--- protobuf_parse.pl
+++ protobuf_parse.pl
@@ -2,28 +2,64 @@ use warnings;
 use strict;
 use Data::Dumper;
 
+our %field_types = (
+	0 => \&parse_varint,
+	1 => '64-bit',
+	2 => \&parse_length_determined,
+	5 => '32-bit',
+);
+
 open(my $fh, '<:raw', $ARGV[0]) or die "Forgot input";
 
 my $line = <$fh>;
+
+close($fh);
+
 chomp $line;
 parse($line);
 
-our %field_types = (
-	0 => 'varint',
-	1 => '64-bit',
-	2 => 'length determined',
-	5 => '32-bit',
-)
-
 sub parse
 {
-	my @bytes = map { unpack('H2') } split(//, $_[0]);
+	my @row = ();
+	my @bytes = map { ord } split(//, $_[0]);
 
-	foreach my $byte (@bytes)
+	while(@bytes)
 	{
-		my ($field_number, $wiretype) = parse
-	}
-	print Dumper(@bytes);
-	# print join("\n", @bytes);
+		my $val = shift @bytes;
+		my $wireType = $val & 7;
+		my $fieldNum = $val >> 3;
 
+		$row[$fieldNum] = $::field_types{$wireType}->(\@bytes);
+		last;
+	}
+	
+	print Dumper(\@row);
+}
+
+sub parse_varint
+{
+	my @bytes = @{ $_[0] };
+	my @stack = ();
+	my $decimal = 0;
+
+	# Pop values until the MSB is no longer set
+	while(my $val = shift @bytes)
+	{
+		push(@stack, $val);
+		if(!($val & 0b10000000)){ last; }
+	}
+
+	# Reverse stack to put it in the correct order
+	@stack = reverse(@stack);
+
+	# Translate values to decimal
+	while(my $val = shift @stack)
+	{
+		# Drop the the MSB
+		$val &= 0b01111111;
+
+		$decimal += $val * 128**(scalar @stack);
+	}
+
+	return $decimal;
 }
